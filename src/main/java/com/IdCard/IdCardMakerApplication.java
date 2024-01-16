8ef88a93fbd06f656f5fb36b954b960c49a5748d
Abir Sarkar <abirsarkar1999@gmail.com>
2024-01-16 14:22:38 +0530

    This is the first commit with the automated code

---
+++ src/main/java/com/IdCard/IdCardMakerApplication.java
@@ -0,0 +1,13 @@
+package com.IdCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdCardMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdCardMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IdCard/IdCardMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IdCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdCardMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
